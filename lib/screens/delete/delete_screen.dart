51f92a39017a3446dad1e4276673d6b8a611e30f
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-01-08 01:45:33 +0700

    feat: delete screen

--- lib/screens/delete/delete_screen.dart
+++ lib/screens/delete/delete_screen.dart
@@ -1,9 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_flutter_firebase_crud/constants/collection.dart';
+import 'package:simple_flutter_firebase_crud/models/user.dart';
 
-class DeleteScreen extends StatelessWidget {
+class DeleteScreen extends StatefulWidget {
   const DeleteScreen({Key? key}) : super(key: key);
 
   static const routeName = "/delete";
+
+  @override
+  State<DeleteScreen> createState() => _DeleteScreenState();
+}
+
+class _DeleteScreenState extends State<DeleteScreen> {
+  late List<QueryDocumentSnapshot<Map<String, dynamic>>> _userIds;
+  bool _isLoading = true;
+
+  Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>> _fetchData() async {
+    final db = FirebaseFirestore.instance;
+    late List<QueryDocumentSnapshot<Map<String, dynamic>>> result;
+    await db.collection(Collection.users).get().then((value) {
+      result = value.docs;
+    }).onError((error, stackTrace) {
+      print(error);
+    });
+    return result;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _isLoading = true;
+    _fetchData().then((value) {
+      _userIds = value;
+      Future.delayed(const Duration(milliseconds: 1000)).then((_) {
+        setState(() {
+          print("finished");
+          print(_userIds);
+          _isLoading = false;
+        });
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +49,32 @@ class DeleteScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Delete'),
       ),
-      body: Container(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: _isLoading
+              ? const CircularProgressIndicator()
+              : ListView.builder(
+                  itemCount: _userIds.length,
+                  itemBuilder: (context, index) {
+                    print(_userIds[index].data());
+                    User user = User.fromJson(_userIds[index].data());
+                    return _buildUserItem(user);
+                  },
+                ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserItem(User user) {
+    return ListTile(
+      title: Text(user.username),
+      subtitle: Text("Validated: ${user.validated}"),
+      trailing: IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.delete),
+      ),
     );
   }
 }
